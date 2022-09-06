9a24b1460046272c0a06b32e817ae6a342025f99
Vi-user <vi.volkova.2712@gmail.com>
2022-09-06 09:14:29 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -38,6 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    let arrOfSymbols = [];
+    let res = '';
+    for (let i = 0; i < expr.length/10; i++) {
+        arrOfSymbols.push(expr.slice(i*10, i*10+10))
+    }
+    arrOfSymbols.forEach(el => {
+        if (el[0] === '*') return res += ' ';
+        const letterSymbols = [];
+        for (let i = 0; i < 5; i++) {
+            if (el.slice(i*2, i*2+2) === '10') letterSymbols.push('.')
+            if (el.slice(i*2, i*2+2) === '11') letterSymbols.push('-')
+        }
+        res += MORSE_TABLE[letterSymbols.join('')]
+    })
+    return res;
     // write your solution here
 }
 
